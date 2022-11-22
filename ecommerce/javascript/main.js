77c71788da69ac34f07212a2dd55bba5694c2877
Franm0rales <PICASSOMORALES@GMAIL.COM>
2022-11-22 11:29:39 +0100

    actu

--- ecommerce/javascript/main.js
+++ ecommerce/javascript/main.js
@@ -29,7 +29,7 @@ function productos() {
         <div class="figure">
         <div class="card-content">
           <figure>
-              <img src="../imagenlogo/ms2.png" alt="Portatil" width="200px" height="150px" >
+              <img src="../imagenlogo/ms2.jpg" alt="Portatil" width="200px" height="150px" >
           </figure></div>
       
           <div class="figurecard">
@@ -53,7 +53,7 @@ function productos() {
                 <a onclick="agregarCarrito()" class ="buttonenlace">
                 
                 <span> 🛒 Carrito</span></a></div>
-              <a href="todoslosproductos.html" class="buttonenlace">Ver</a>
+              <a onclick="ofertas()" id="ofertas" href="todoslosproductos.html?id=${response[i].nombre}" class="buttonenlace">Ver</a>
           </div>
           </div>
         </div>`;
@@ -62,5 +62,59 @@ function productos() {
     }
   };
   request.open("GET", "http://localhost:8000/ProductosDestacados", true);
+  request.send();
+}
+function ofertas() {
+  let userId = new URLSearchParams(window.location.search).get("id");
+  console.log(userId);
+  const request = new XMLHttpRequest();
+  request.onreadystatechange = function () {
+    console.log(`Estado actual ${this.readyState}`);
+    if (this.readyState == 4 && this.status == 200) {
+      let response = JSON.parse(this.responseText);
+      let userDiv = document.getElementById("ofertas");
+      let htmlContent = "";
+      for (i = 0; i < response.length; i++) {
+        if(userId===response[i].nombre){
+          
+        htmlContent +=`<article class=" wrap card">
+        <div class="figure">
+        <div class="card-content">
+          <figure>
+              <img src="../imagenlogo/ms2.jpg" alt="Portatil" width="200px" height="150px" >
+          </figure></div>
+      
+          <div class="figurecard">
+        <div class="contentproducto">
+            <div class="tituloarticulo">
+              <h4>${response[i].nombre}</h4></div>
+              <div class="precio">
+                <span><b>${response[i].precio}</b></span></div>
+                <div class="divisa">
+                <span>€</span></div>
+                <div class="rating">
+                  <span class="filled">★</span>
+                  <span class="filled">★</span>
+                  <span class="filled">★</span>
+                  <span>★</span>
+                  <span>★</span>
+                </div>
+              <h6>${response[i].descripcion}</h6>
+              <h6>${response[i].Especificaciones}</h6>
+      
+              <div class="carrito">
+                <a onclick="agregarCarrito()" class ="buttonenlace">
+                
+                <span> 🛒 Carrito</span></a></div>
+              <a onclick="ofertas()" href="todoslosproductos.html" class="buttonenlace">Ver</a>
+          </div>
+          </div>
+        </div>`
+        }
+      }
+      userDiv.innerHTML = htmlContent;
+    }
+  };
+  request.open("GET", "http://localhost:8000/ofertas", true);
   request.send();
 }
--- nodejs/app.js
+++ nodejs/app.js
@@ -7,28 +7,70 @@ app.get("/", function (request, response) {
 });
 app.get("/ProductosDestacados", function (request, response) {
   const productoUno = {
-    nombre: "portatil Acer",
+    nombre: "Portatil Acer",
     stock: 100,
     precio: 1499.99,
   };
   const productoDos = {
-    nombre: "portatil Msi",
+    nombre: "Portatil Msi",
     stock: 100,
     precio: 1499.99,
   };
   const productoTres = {
-    nombre: "portatil Msi",
+    nombre: "Portatil Msi",
     stock: 100,
     precio: 1499.99,
   };
   const productoCuatro = {
-    nombre: "portatil Msi",
+    nombre: "Portatil Acer",
     stock: 100,
     precio: 1499.99,
   };
   let productos = [productoUno, productoDos, productoTres, productoCuatro];
   response.send(productos);
 });
+app.get("/ofertas", function (request, response){
+  const ofertaUno = {
+    nombre: "Portatil Acer",
+    stock: 100,
+    precio: 1499.99,
+    descripcion:"Acelere a fondo con la computadora portátil para juegos Acer Nitro 5 AMD Ryzen™ Serie 6000 en modelos de 15 o 17 pulgadas,ya sea que la use como computadora portátil para juegos casuales o para subir de nivel en línea.",
+    Especificaciones:"Procesador AMD Ryzen™ 7 6800H 3.2 GHz Memoria 16 GB DDR5 Memory Controlador gráfico NVIDIA® GeForce RTX™ 3050 4G-GDDR6(2C*256*16*4) Almacenamiento 512GB PCIe NVMe SED SSD Unidad óptica NO Display 15.6 FHD IPS 144Hz SlimBezel 16:9 Controlador gráfico NVIDIA® GeForce RTX™ 3050 4G-GDDR6(2C*256*16*4)"
+    
+  
+  };
+  const ofertaDos = {
+    nombre: "Portatil Msi",
+    stock: 100,
+    precio: 1499.99,
+    descripcion:"Acelere a fondo con la computadora portátil para juegos Acer Nitro 5 AMD Ryzen™ Serie 6000 en modelos de 15 o 17 pulgadas,ya sea que la use como computadora portátil para juegos casuales o para subir de nivel en línea.",
+    Especificaciones:"Procesador AMD Ryzen™ 7 6800H 3.2 GHz Memoria 16 GB DDR5 Memory Controlador gráfico NVIDIA® GeForce RTX™ 3050 4G-GDDR6(2C*256*16*4) Almacenamiento 512GB PCIe NVMe SED SSD Unidad óptica NO Display 15.6 FHD IPS 144Hz SlimBezel 16:9 Controlador gráfico NVIDIA® GeForce RTX™ 3050 4G-GDDR6(2C*256*16*4)"
+    
+  
+  };
+  const ofertaTres = {
+    nombre: "Portatil Msi",
+    stock: 100,
+    precio: 1499.99,
+    descripcion:"Acelere a fondo con la computadora portátil para juegos Acer Nitro 5 AMD Ryzen™ Serie 6000 en modelos de 15 o 17 pulgadas,ya sea que la use como computadora portátil para juegos casuales o para subir de nivel en línea.",
+    Especificaciones:"Procesador AMD Ryzen™ 7 6800H 3.2 GHz Memoria 16 GB DDR5 Memory Controlador gráfico NVIDIA® GeForce RTX™ 3050 4G-GDDR6(2C*256*16*4) Almacenamiento 512GB PCIe NVMe SED SSD Unidad óptica NO Display 15.6 FHD IPS 144Hz SlimBezel 16:9 Controlador gráfico NVIDIA® GeForce RTX™ 3050 4G-GDDR6(2C*256*16*4)"
+    
+  
+  };
+  const ofertaCuatro = {
+    nombre: "Portatil Acer",
+    stock: 100,
+    precio: 1499.99,
+    descripcion:"Acelere a fondo con la computadora portátil para juegos Acer Nitro 5 AMD Ryzen™ Serie 6000 en modelos de 15 o 17 pulgadas,ya sea que la use como computadora portátil para juegos casuales o para subir de nivel en línea.",
+    Especificaciones:"Procesador AMD Ryzen™ 7 6800H 3.2 GHz Memoria 16 GB DDR5 Memory Controlador gráfico NVIDIA® GeForce RTX™ 3050 4G-GDDR6(2C*256*16*4) Almacenamiento 512GB PCIe NVMe SED SSD Unidad óptica NO Display 15.6 FHD IPS 144Hz SlimBezel 16:9 Controlador gráfico NVIDIA® GeForce RTX™ 3050 4G-GDDR6(2C*256*16*4)"
+    
+  
+  };
+  let ofertas = [ofertaUno, ofertaDos, ofertaTres,ofertaCuatro];
+  response.send(ofertas);
+
+});
+
 app.listen(8000, function () {
   console.log("API lista para recibir llamadas");
 });
